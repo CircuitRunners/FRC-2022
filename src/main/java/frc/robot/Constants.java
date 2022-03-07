4c13f39972b9be300e0cb6c0f7418a34eaf7df5b
Evan Meltzer <50961365+Sate04@users.noreply.github.com>
2022-03-07 16:03:21 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+public class Constants {
+    public static final int FLIGHT_Y_AXIS = 1; // REVERSE THIS VALUE
+    public static final int FLIGHT_X_AXIS = 0;
+    public static final int FLIGHT_Z_AXIS = 2;
+
+
+    public static final int XBOX_L_X_AXIS = 0;
+    public static final int XBOX_L_Y_AXIS = 1; // REVERSE THIS VALUE
+    public static final int XBOX_R_X_AXIS = 4;
+    public static final int XBOX_R_Y_AXIS = 5; // REVERSE THIS VALUE
+    public static final int XBOX_L_TRIGGER_AXIS = 2;
+    public static final int XBOX_R_TRIGGER_AXIS = 3;
+
+    public static final int XBOX_A_BUTTON = 1;
+    public static final int XBOX_B_BUTTON = 2;
+    public static final int XBOX_X_BUTTON = 3;
+    public static final int XBOX_Y_BUTTON = 4;
+    public static final int XBOX_LBUMPER_BUTTON = 5;
+    public static final int XBOX_RBUMPER_BUTTON = 6;
+    public static final int XBOX_BACK_BUTTON = 7;
+    public static final int XBOX_START_BUTTON = 8;
+    public static final int XBOX_LSTICK_BUTTON = 9;
+    public static final int XBOX_RSTICK_BUTTON = 10;
+}
