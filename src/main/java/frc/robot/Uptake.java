6712e3686d6dc57b8d8f6c6baef07eb8bcad743f
wackbyte <wackbyte@pm.me>
2022-03-08 16:10:11 -0500

    if both uptake controls are pressed nothing happens
    + speed variable

--- src/main/java/frc/robot/Uptake.java
+++ src/main/java/frc/robot/Uptake.java
@@ -13,45 +13,41 @@ public class Uptake
      */
     private VictorSP buptakeMotor;
 
+    private double speed = 0.5;
+
     /**
      * Uptake Constructor, instantiates uptake motor controllers
      *
      * @param topIndex The PWM index of the top motor controller
      * @param bottomIndex The PWM index of the bottom motor controller
      */
-    public Uptake(int topIndex, int bottomIndex){
+    public Uptake(int topIndex, int bottomIndex) {
         uptakeMotor = new VictorSP(topIndex);
         buptakeMotor = new VictorSP(bottomIndex);
     }
 
-    public void runAutonomousStart(){
-        uptakeMotor.set(.5);
-        buptakeMotor.set(.5);
+    public void runAutonomousStart() {
+        uptakeMotor.set(speed);
+        buptakeMotor.set(speed);
     }
 
-    public void runAutonomousEnd(){
+    public void runAutonomousEnd() {
         uptakeMotor.stopMotor();
         buptakeMotor.stopMotor();
     }
     
     public void runMotor(Boolean aButton, Boolean bButton)
     {
-        if (aButton)
-        {
-            uptakeMotor.set(.5);
-            buptakeMotor.set(.5);
+        double axis = 0;
+        if (aButton) {
+            axis += 1;
+        }
+        if (bButton) {
+            axis -= 1;
         }
 
-        else if (bButton)
-        {
-            uptakeMotor.set(-.5);
-            buptakeMotor.set(-.5);
-        }
-        else
-        {
-            uptakeMotor.set(0);
-            buptakeMotor.set(0);
-        }
+        uptakeMotor.set(axis * speed);
+        buptakeMotor.set(axis * speed);
     }
 }
 
